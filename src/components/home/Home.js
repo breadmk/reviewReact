c2164069ce776dfa34cee49ceb3e8a27966e10db
breadmk <minvita9999@naver.com>
2021-12-14 16:23:35 +0900

    prop -ing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Home = () => {
-   return (
-      <div>
-         <h1>홈페이지입니다.</h1>
-      </div>
-   );
+//Function 방식
+//Class 방식
+const Home = (boards) => {
+	return (
+		<div>
+			<h1>홈페이지입니다.</h1>
+		</div>
+	);
 };
 
 export default Home;
